927f3dd761ec2639471ca6194b0db97f10a61f21
Karthikeya-Akhandam <abkarthikeya2005@gmail.com>
2026-06-03 11:04:40 +0530

    Commit 4: Create Extension background worker placeholder file

---
+++ extension/src/background/background.ts
@@ -0,0 +1,7 @@
+// AlgoLens Background Service Worker
+console.log('AlgoLens background worker initialized.');
+
+// Placeholder listener for extension install/setup
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('AlgoLens extension installed successfully.');
+});
